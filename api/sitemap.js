af2e08ccd00f7077f1ebf862369d8f91a4fdc402
Siamak Kalhor <siamakk2@users.noreply.github.com>
2026-07-30 04:16:46 +0000

    Sitemap: correct edge cache policy (s-maxage + stale-while-revalidate)

--- api/sitemap.js
+++ api/sitemap.js
@@ -62,6 +62,5 @@ export default async function handler(req, res) {
 
   res.setHeader("Content-Type", "application/xml; charset=utf-8");
   res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=86400");
-  res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=86400");
   return res.status(200).send(xml);
 }
